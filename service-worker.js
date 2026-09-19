c16a8a27565b5cd44ccd30d4e09cf4a2c77d4954
Lance99501 <108451950+Lance99501@users.noreply.github.com>
2026-09-19 14:25:30 +0800

    Refresh PWA cache for Segovia update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION='spain2026-20260918-v49';
+const CACHE_VERSION='spain2026-20260919-v50';
 const APP_CACHE=`${CACHE_VERSION}-app`;
 const RUNTIME_CACHE=`${CACHE_VERSION}-runtime`;
 const TILE_CACHE=`${CACHE_VERSION}-tiles`;
